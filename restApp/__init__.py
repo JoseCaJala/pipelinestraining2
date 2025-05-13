a2027c638b94abbe8b23091bf9a1c5a82601b754
jose <jernestocastro01@gmail.com>
2025-05-13 10:09:04 -0400

    Practice 4 pipelinetraining 4

--- restApp/__init__.py
+++ restApp/__init__.py
@@ -3,8 +3,10 @@ import time
 import redis
 from flask import Flask
 
+
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+
 
 def get_hit_count():
     retries = 5
@@ -16,6 +18,7 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
+
 
 @app.route('/')
 def hello():
